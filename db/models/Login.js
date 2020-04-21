2df7eb93f7fef3a5ad4ab7b8112dc275ec90aa95
Galen Emanuel <galen.emanuel.fencing@gmail.com>
2020-04-21 16:28:31 -0400

    seed logic fleshed out

--- db/models/Login.js
+++ db/models/Login.js
@@ -2,9 +2,11 @@ const mongoose = require('../db/connection.js');
 const Schema = mongoose.Schema;
 
 const Login = new Schema({
+    UserID: {type: Schema.Types.ObjectId, ref: 'User'},
     Token: String,
     Username: String,
-    Password: String
+    Password: String,
+    Admin: Boolean
 })
 
 module.exports = mongoose.model('Login', Login)
--- db/seed.js
+++ db/seed.js
@@ -1,18 +1,35 @@
-const User = require('./models/User')
+const db = require('./models')
 
 const testusers = ["galen", 'daniel', 'shimin']
 
 const accounts;
-const logins;
+const logins = [
+    {Token: 'galengit'}, {Token: 'danielgit'}, {Token:'shimingit'}];
+
 // should delete all users in database and for each test user
 // create and populate a user 
 // must also create the reference objects in separate collections of the db
 // so that population is possible
-User.deleteMany({}).then(() =>{
-  User.create(testUserData).then(responseData=>{
-    console.log(responseData)
-    process.exit();
-  }).catch(err =>{
-    console.log(err)
-  })
+
+db.User.deleteMany({}).then(() =>{
+    //necessary user elements: account
+    //left to routes: all populating, Array 
+    //
+    for (let i = 0; i < testusers; i++){
+        let login = new db.Login({
+            Username: testusers[i], 
+            Token: logins[i]
+        })
+        let account = new db.Account({
+
+        })
+        let user = {
+            Login : login._id,
+            Account : account._id
+        }
+        db.User.create(user).then(responseData=>{
+            console.log(responseData);
+            process.exit()
+        })
+    }   
 })
