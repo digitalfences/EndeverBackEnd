ead45756ce34d68862ca4af600766d8ae772a349
Galen Emanuel <galen.emanuel.fencing@gmail.com>
2020-04-23 16:05:31 -0400

    changed configs

--- db/configs.js
+++ db/configs.js
@@ -1,7 +1,7 @@
 module.exports = {
   GITHUB_CLIENT_ID: "fda597fe607c7161f2a0",
   GITHUB_CLIENT_SECRET: "620b1562c7c1cc57112fb3b54a2978df22f98e37",
-  GITHUB_CALLBACK_URL: "http://localhost:4000/auth/github/callback",
+  GITHUB_CALLBACK_URL: "https://tigerkingbackend/auth/github/callback",
   FRONTEND_URL: "http://localhost:3000",
   PORT: 4000,
 };
