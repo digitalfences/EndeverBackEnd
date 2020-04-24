ca1caea5673538cb535183fea4c027c9823aa19b
shimin <thunderous_0518@hotmail.com>
2020-04-24 17:23:38 -0400

    deploy to heroku

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,15 @@
-const mongoose = require('mongoose');
-let mongooseURI = '';
-if (process.env.NODE_ENV === "production"){
-    mongooseURI = process.env.DB_URL;
+const mongoose = require("mongoose");
+let mongooseURI = "";
+if (process.env.NODE_ENV === "production") {
+  mongooseURI = process.env.DB_URL;
 } else {
-    mongooseURI = "mongodb://localhost/endever";
+  mongooseURI = "mongodb://localhost/endever";
 }
-
 
 //test
 
-mongoose.connect(mongooseURI, {useNewUrlParser: true});
+mongoose.connect(mongooseURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 module.exports = mongoose;
