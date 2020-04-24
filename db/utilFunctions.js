3949f049d9d61830b9323d7e113fa16e1b60da75
digitalfences <39531435+digitalfences@users.noreply.github.com>
2020-04-23 20:45:10 -0400

    Update utilFunctions.js

--- db/utilFunctions.js
+++ db/utilFunctions.js
@@ -2,60 +2,48 @@ const fetch = require("node-fetch");
 const Login = require("./models/Login");
 const Account = require("./models/Account");
 const User = require("./models/User");
-
-function checkAndSave(userInfo) {
-  let userName = userInfo.login;
-  Login.findOne({ Username: userName }).then((loginUser) => {
-    if (loginUser === null || loginUser === undefined) {
-      let _loginUser = { Username: userName };
-      Login.create(_loginUser).then((_login) => {
-        console.log("user create Success!");
-        let account = { Picture: userInfo.avatar_url };
-        Account.create(account).then((_account) => {
-          let user = { Login: _login._id, Account: _account._id };
-          User.create(user).then((_user) => {
-            return _user;
-          });
-        });
-      });
-    } else {
-      return loginUser;
-    }
-    // set the session
-  });
-}
-
 function checkUserOrSave(profile, done) {
   let userInfo = profile._json;
   let userName = userInfo.login;
-  User.findOne({ UserName: userName }).then((loginUser) => {
-    if (loginUser === null || loginUser === undefined) {
-      // save the new user's data
-      let _loginUser = { Username: userName };
-      Login.create(_loginUser).then((_login) => {
-        console.log("user create Success!");
-        let repos = getRepoFromApiByUserName(userName);
-        console.log("repos");
-        console.log(repos);
-        let account = {
-          Picture: userInfo.avatar_url,
-          Bio: userInfo.bio,
-          Repositories: repos,
-        };
-        Account.create(account).then((_account) => {
-          let user = { UserName: _login.Username, Login: _login._id, Account: _account._id };
-          User.create(user).then((_user) => {
-            return done(null, _user);
+  let repos = [];
+  User.findOne({ UserName: userName })
+    .then((user) => {
+      if (user !== null) {
+        repos = getRepoFromApiByUserName(user.UserName);
+      }
+    })
+    .then(() => {
+      User.findOne({ UserName: userName }).then((loginUser) => {
+        if (loginUser === null || loginUser === undefined) {
+          // save the new user's data
+          let _loginUser = { Username: userName };
+          Login.create(_loginUser).then((_login) => {
+            console.log("user create Success!");
+            console.log("repos");
+            console.log(repos);
+            let account = {
+              Picture: userInfo.avatar_url,
+              Bio: userInfo.bio,
+              Repositories: repos,
+            };
+            Account.create(account).then((_account) => {
+              let user = {
+                UserName: _login.Username,
+                Login: _login._id,
+                Account: _account._id,
+              };
+              User.create(user).then((_user) => {
+                return done(null, _user);
+              });
+            });
           });
-        });
+        } else {
+          return done(null, loginUser);
+        }
+        // set the session
       });
-    } else {
-      return done(null, loginUser);
-    }
-    // set the session
-  });
+    });
 }
-
 function getRepoFromApiByUserName(userName) {
   let reposUrl = "https://api.github.com/users/" + userName + "/repos";
   console.log(reposUrl);
@@ -82,5 +70,4 @@ function getRepoFromApiByUserName(userName) {
       return [];
     });
 }
-
 module.exports = { checkAndSave, checkUserOrSave, getRepoFromApiByUserName };
