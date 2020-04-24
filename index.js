564d8869e94e3ea3306b0bea75daf8e753de9047
shimin <thunderous_0518@hotmail.com>
2020-04-24 14:13:48 -0400

    edit

--- index.js
+++ index.js
@@ -48,7 +48,8 @@ app.use(function (req, res, next) {
   //res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Origin",
-    "https://tigerkingfront.netlify.app"
+    // "https://tigerkingfront.netlify.app"
+    "*"
   );
   next();
 });
