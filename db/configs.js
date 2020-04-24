8996dfd0465cbb3671abe6c7263fa849e0464cb0
shimin <thunderous_0518@hotmail.com>
2020-04-24 13:54:11 -0400

    edit the configuration

--- db/configs.js
+++ db/configs.js
@@ -1,7 +1,8 @@
 module.exports = {
   GITHUB_CLIENT_ID: "fda597fe607c7161f2a0",
   GITHUB_CLIENT_SECRET: "620b1562c7c1cc57112fb3b54a2978df22f98e37",
-  GITHUB_CALLBACK_URL: "https://tigerkingbackend.herokuapp.com/auth/github/callback",
-  FRONTEND_URL: "https://agitated-panini-b410aa.netlify.app",
+  GITHUB_CALLBACK_URL:
+    "https://tigierkingbackendv2.herokuapp.com/auth/github/callback",
+  FRONTEND_URL: "https://tigerkingfront.netlify.app",
   PORT: 4000,
 };
--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use(function (req, res, next) {
   //res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Origin",
-    "https://agitated-panini-b410aa.netlify.app"
+    "https://tigerkingfront.netlify.app"
   );
   next();
 });
