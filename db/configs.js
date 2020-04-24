a59ae1a0b9af4df667a59bd2375854cc1f7e05d1
Galen Emanuel <galen.emanuel.fencing@gmail.com>
2020-04-24 11:34:46 -0400

    minor launch fix herokue

--- db/configs.js
+++ db/configs.js
@@ -2,6 +2,6 @@ module.exports = {
   GITHUB_CLIENT_ID: "fda597fe607c7161f2a0",
   GITHUB_CLIENT_SECRET: "620b1562c7c1cc57112fb3b54a2978df22f98e37",
   GITHUB_CALLBACK_URL: "https://tigerkingbackend.herokuapp.com/auth/github/callback",
-  FRONTEND_URL: "http://localhost:3000",
+  FRONTEND_URL: "https://agitated-panini-b410aa.netlify.app",
   PORT: 4000,
 };
