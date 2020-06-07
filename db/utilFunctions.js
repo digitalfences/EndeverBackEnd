69c96a6802517f8660cfa4692336da4009ab094f
shimin <thunderous_0518@hotmail.com>
2020-06-07 11:21:05 -0400

    merge

--- db/utilFunctions.js
+++ db/utilFunctions.js
@@ -14,7 +14,6 @@ function shuffle(sourceArray) {
   return sourceArray;
 }
 function checkUserOrSave(profile, done) {
-<<<<<<< HEAD
   let userInfo = profile._json;
   let userName = userInfo.login;
   let reposUrl = "https://api.github.com/users/" + userName + "/repos";
@@ -71,53 +70,5 @@ function checkUserOrSave(profile, done) {
         // end of then
       });
     });
-=======
-    let userInfo = profile._json;
-    let userName = userInfo.login;
-    let reposUrl = "https://api.github.com/users/" + userName + "/repos";
-    let repos = [];
-    fetch(reposUrl).then(res => res.json()).then(res => {
-        let repoList = [];
-        res.map((item) => {
-            let repoUrl = item.name;
-            if (repoUrl !== undefined && repoUrl !== "") {
-                repoList.push(repoUrl);
-            }
-        });
-        User.findOne({ UserName: userName }).populate('Account').then((loginUser) => {
-            if (loginUser === null || loginUser === undefined) {
-                let _loginUser = {
-                    Username: userName
-                };
-                Login.create(_loginUser).then((_login) => {
-                    console.log("user create Success!");
-                    console.log("repos");
-                    console.log(repoList);
-
-                    let account = {
-                        RealName: userInfo.name,
-                        WorkPlace: userInfo.company,
-                        Picture: userInfo.avatar_url,
-                        Bio: userInfo.bio,
-                        Repositories: repoList,
-                    };
-                    Account.create(account).then((_account) => {
-                        let user = {
-                            UserName: _login.Username,
-                            Login: _login._id,
-                            Account: _account._id
-                        };
-                        User.create(user).populate('Account').then((_user) => {
-                            return done(null, _user);
-                        });
-                    });
-                })
-            } else {
-                return done(null, loginUser);
-            }
-            // end of then
-        });
-    })
->>>>>>> master
 }
 module.exports = { checkUserOrSave, shuffle };
