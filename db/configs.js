c2993ed497c4482d92a19e92c8682c66665119e4
galen.emanuel.fencing@gmail.com <galen.emanuel.fencing@gmail.com>
2021-04-19 02:12:06 -0400

    Fixed wrong pointing URI

--- db/configs.js
+++ db/configs.js
@@ -1,7 +1,7 @@
 module.exports = {
   GITHUB_CLIENT_ID: "fda597fe607c7161f2a0",
   GITHUB_CLIENT_SECRET: "620b1562c7c1cc57112fb3b54a2978df22f98e37",
-  GITHUB_CALLBACK_URL: "https://tigerkingbackend.herokuapp.com/auth/github/callback",
+  GITHUB_CALLBACK_URL: "https://endever.herokuapp.com/auth/github/callback",
   //GITHUB_CALLBACK_URL: "http://localhost:4000/auth/github/callback",
   FRONTEND_URL: "https://agitated-panini-b410aa.netlify.app",
   //FRONTEND_URL: "http://localhost:3000",
